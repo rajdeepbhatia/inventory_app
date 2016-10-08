6b12ccbbf914baa1352080bf97fb67ac54d8c74f
rajdeepbhatia <rajdeep.bhatia@mindfiresolutions.com>
2016-10-09 04:00:27 +0530

    Add authenticated header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :set_layout
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
   def set_layout
     if user_signed_in?
       'application'
     else
       'unauthenticated'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
   end
 end
